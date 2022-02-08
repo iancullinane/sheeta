00e455a489a7702d2e92b6ad36be337e371d7d67
Ian Cullinane <ian.cullinane@gmail.com>
2022-02-08 09:41:14 -0500

    test api proxy adapter

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/iancullinane/sheeta
 go 1.13
 
 require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/aws/aws-lambda-go v1.28.0
 	github.com/aws/aws-sdk-go v1.30.14
+	github.com/awslabs/aws-lambda-go-api-proxy v0.12.0 // indirect
 	github.com/bsdlp/discord-interactions-go v0.0.0-20201227083222-a2ba84473ce8 // indirect
 	github.com/bwmarrin/discordgo v0.23.3-0.20220202194601-aba5dc811da8
 	github.com/golang/mock v1.4.3
@@ -14,5 +16,5 @@ require (
 	github.com/urfave/cli/v2 v2.2.0
 	golang.org/x/crypto v0.0.0-20220131195533-30dcbda58838 // indirect
 	golang.org/x/sys v0.0.0-20220207234003-57398862261d // indirect
-	gopkg.in/yaml.v2 v2.2.2
+	gopkg.in/yaml.v2 v2.3.0
 )
--- src/main.go
+++ src/main.go
@@ -6,17 +6,13 @@ import (
 	"crypto/ed25519"
 	"encoding/hex"
 	"flag"
+	"io"
 	"log"
-	"os"
+	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ssm"
-	"github.com/bwmarrin/discordgo"
-	"github.com/iancullinane/sheeta/src/application"
-	"github.com/iancullinane/sheeta/src/internal/services"
+	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
 )
 
 // // Variables used for command line parameters
@@ -76,154 +72,51 @@ func init() {
 //
 func main() {
 
-	sess := session.Must(session.NewSession())
-	// AWS config for client creation
-	awsConfigUsEast1 := &aws.Config{
-		CredentialsChainVerboseErrors: aws.Bool(true),
-		S3ForcePathStyle:              aws.Bool(true),
-		Region:                        aws.String("us-east-1"), // us-east-2 is the destination bucket region
-	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello")
+	})
 
-	// Create service client value configured for credentials
-	// from assumed role.
-	// s3svc := s3manager.NewDownloader(sess)
-	// cfnSvc := cloudformation.New(sess, awsConfigUsEast2)
-	ssmStore := ssm.New(sess, awsConfigUsEast1)
-	dToken, err := services.GetParameter(ssmStore, aws.String("/discord/sheeta/token"))
-	if err != nil {
-		panic(err)
-	}
+	lambda.Start(httpadapter.NewV2(http.DefaultServeMux).ProxyWithContext)
 
-	apiID, err := services.GetParameter(ssmStore, aws.String("/discord/sheeta/app-id"))
-	if err != nil {
-		panic(err)
-	}
+	// sess := session.Must(session.NewSession())
+	// // AWS config for client creation
+	// awsConfigUsEast1 := &aws.Config{
+	// 	CredentialsChainVerboseErrors: aws.Bool(true),
+	// 	S3ForcePathStyle:              aws.Bool(true),
+	// 	Region:                        aws.String("us-east-1"), // us-east-2 is the destination bucket region
+	// }
 
-	log.Println("token value")
-	log.Printf("%#v", *dToken.Parameter.Value)
-	d, err := discordgo.New("Bot " + *dToken.Parameter.Value)
-	if err != nil {
-		panic(err)
-	}
+	// // Create service client value configured for credentials
+	// // from assumed role.
+	// // s3svc := s3manager.NewDownloader(sess)
+	// // cfnSvc := cloudformation.New(sess, awsConfigUsEast2)
+	// ssmStore := ssm.New(sess, awsConfigUsEast1)
+	// dToken, err := services.GetParameter(ssmStore, aws.String("/discord/sheeta/token"))
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	if RunSlashBuilder == "create" {
-		log.Println("api value")
-		log.Printf("%#v", *apiID.Parameter.Value)
-		err := application.CreateSlashCommands(*apiID.Parameter.Value, d)
-		if err != nil {
-			log.Println(err)
-		}
-		os.Exit(0)
-	}
+	// apiID, err := services.GetParameter(ssmStore, aws.String("/discord/sheeta/app-id"))
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	lambda.Start(HandleRequest)
+	// log.Println("token value")
+	// log.Printf("%#v", *dToken.Parameter.Value)
+	// d, err := discordgo.New("Bot " + *dToken.Parameter.Value)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// if RunSlashBuilder == "create" {
+	// 	log.Println("api value")
+	// 	log.Printf("%#v", *apiID.Parameter.Value)
+	// 	err := application.CreateSlashCommands(*apiID.Parameter.Value, d)
+	// 	if err != nil {
+	// 		log.Println(err)
+	// 	}
+	// 	os.Exit(0)
+	// }
+
+	// lambda.Start(HandleRequest)
 }
-
-// // Variables used for command line parameters
-// var (
-// 	Token string
-// )
-
-// // For command line startup
-// // TODO::Container, cloud, blah blah blah
-// func init() {
-// 	flag.StringVar(&Token, "t", "", "Bot Token")
-// 	flag.Parse()
-// }
-
-// func main() {
-
-// 	// Set up logger to be used by package clients
-// 	logger := logrus.New()
-// 	logger.Level = logrus.InfoLevel
-// 	logger.Out = os.Stdout
-
-// 	// Set up config
-// 	var conf *config.Config
-// 	conf = conf.BuildConfigFromFile("./src/config/base.yaml")
-
-// 	sess := session.Must(session.NewSession())
-// 	// AWS config for client creation
-// 	awsConfigUsEast2 := &aws.Config{
-// 		CredentialsChainVerboseErrors: aws.Bool(true),
-// 		S3ForcePathStyle:              aws.Bool(true),
-// 		Region:                        aws.String("us-east-2"), // us-east-2 is the destination bucket region
-// 	}
-
-// 	// Create service client value configured for credentials
-// 	// from assumed role.
-// 	s3svc := s3manager.NewDownloader(sess)
-// 	cfnSvc := cloudformation.New(sess, awsConfigUsEast2)
-
-// 	// This effectively defines what aws services are available
-// 	// TODO::I want to move this into its module, but it causes tests to break
-// 	// because of a region error related to the credential chain
-// 	cr := cloud.Services{
-// 		S3: s3svc,
-// 		CF: cfnSvc,
-// 	}
-
-// 	var bot []bot.Module
-// 	c := cloud.NewCloud(cr, conf.GetValueMap())
-// 	bot = append(bot, c)
-
-// 	// Create a new Discord session using the provided bot token.
-// 	d, err := discordgo.New("Bot " + Token)
-// 	if err != nil {
-// 		logger.Fatalf("Could not start bot: %s", err)
-// 	}
-
-// 	// Register modules handlers to discord bot
-// 	for _, mod := range bot {
-// 		d.AddHandler(mod.ExportHandler())
-// 	}
-
-// 	// Open a websocket connection to Discord and begin listening.
-// 	err = d.Open()
-// 	if err != nil {
-// 		fmt.Println("error opening connection,", err)
-// 		return
-// 	}
-
-// 	// Wait here until CTRL-C or other term signal is received.
-// 	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
-// 	sc := make(chan os.Signal, 1)
-// 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
-// 	<-sc
-
-// 	// Cleanly close down the Discord session.
-// 	d.Close()
-// }
-
-//
-//
-//
-//
-
-// logger := logrus.New()
-// logger.Level = logrus.InfoLevel
-// logger.Out = os.Stdout
-
-// if config.GetVerbose() {
-// 	logger.Level = logrus.DebugLevel
-// }
-
-// logger.Infof("%s %s (%s, %s)", runtimeConfig.GetEnvironment(), runtimeConfig.GetServiceName(), VersionString, runtime.Version())
-
-// datadogAPIKey := config.GetDatadogAPIKey()
-// datadogAppKey := config.GetDatadogApplicationKey()
-// bb := config.GetBackupBucketName()
-
-// sess := session.Must(session.NewSession(&aws.Config{Region: aws.String(config.GetRegion())}))
-// uploader := s3manager.NewUploader(sess)
-
-// clock := clock.New()
-
-// ddc := datadog.NewClient(datadogAPIKey, datadogAppKey)
-// r := handler.Resources{
-// 	DD:         ddc,
-// 	Uploader:   uploader,
-// 	Logger:     logger,
-// 	BucketName: bb,
-// 	Clock:      clock,
-// }
